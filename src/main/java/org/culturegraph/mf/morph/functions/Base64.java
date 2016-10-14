d19ae2235c330469c87c4044a1e4833aec8ae5ec
Thomas Gängler <thomas.gaengler@slub-dresden.de>
2016-10-14 14:15:26 +0200

    [DD-1401] added Base64 function

---
+++ src/main/java/org/culturegraph/mf/morph/functions/Base64.java
@@ -0,0 +1,22 @@
+package org.culturegraph.mf.morph.functions;
+
+import static java.util.Base64.getEncoder;
+
+/**
+ * Created by tgaengler on 14.10.16.
+ */
+public class Base64 extends AbstractSimpleStatelessFunction {
+
+	private static final java.util.Base64.Encoder ENCODER = getEncoder();
+
+	@Override
+	protected String process(final String value) {
+
+		return generateHash(value);
+	}
+
+	private static String generateHash(final String hashString) {
+
+		return ENCODER.encodeToString(hashString.getBytes());
+	}
+}
